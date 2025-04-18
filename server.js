dfb1b041afd46e8b48b8aa64b8b3bc6271d9c3cb
maferozi <maferozi373@gmail.com>
2025-04-18 17:35:38 +0500

    Add MongoDB connection config

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
