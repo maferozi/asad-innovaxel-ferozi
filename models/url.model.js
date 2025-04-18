49d0fb3f2ad9f6740955d81f32e1e2fed7538d5b
maferozi <maferozi373@gmail.com>
2025-04-18 17:40:17 +0500

    Create URL model schema

---
+++ models/url.model.js
@@ -0,0 +1,12 @@
+// models/url.model.js
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  shortCode: { type: String, required: true, unique: true },
+  accessCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Url', urlSchema);
